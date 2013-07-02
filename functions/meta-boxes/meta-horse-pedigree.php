6b727fdecb609d562e61d70ce3586ecb607a7e8e
Marek Polasek <polasek.marek@gmail.com>
2013-07-02 14:44:47 +0200

    Fix Apostrophe - Horse Pedigree

--- functions/meta-boxes/meta-horse-pedigree.php
+++ functions/meta-boxes/meta-horse-pedigree.php
@@ -1,7 +1,7 @@
 <?php
 
 /*-----------------------------------------------------------------------------------------*/
-/*	Add metabox 
+/*	Add metabox
 /*-----------------------------------------------------------------------------------------*/
 function add_meta_box_horse_pedigree() {
 	add_meta_box('meta_box_horse_pedigree', 'Horse Pedigree', 'meta_box_horse_pedigree', 'horse_post', 'normal', 'high');
@@ -15,36 +15,36 @@ add_action('admin_init', 'add_meta_box_horse_pedigree');
 /*-----------------------------------------------------------------------------------------*/
 function meta_box_horse_pedigree() {
 	global $post;
-	
+
 	// get variables
-	$meta_horse_pedigree = get_post_meta($post->ID,'meta_horse_pedigree',TRUE); 
+	$meta_horse_pedigree = get_post_meta($post->ID,'meta_horse_pedigree',TRUE);
 	if (!$meta_horse_pedigree)$meta_horse_pedigree = array(); // otherwise it creates an error when trying to call the syntax value
-	
+
 	// instead of writing HTML here, lets do an include
 ?>
 
 	<div class="MetaBox">
 		<div id="meta-horse-pedigree">
-			<?php 
+			<?php
 			for ( $x=1; $x<=30; $x++){
 				if ($x == 1) {echo '<div class="MetaHorsePedigreeColumn" id="meta-horse-pedigree-column1"><label>Generation 1</label>';}
 				if ($x == 3) {echo '<div class="MetaHorsePedigreeColumn" id="meta-horse-pedigree-column2"><label>Generation 2</label>';}
 				if ($x == 7) {echo '<div class="MetaHorsePedigreeColumn" id="meta-horse-pedigree-column3"><label>Generation 3</label>';}
 				if ($x == 15) {echo '<div class="MetaHorsePedigreeColumn" id="meta-horse-pedigree-column4"><label>Generation 4</label>';}
 			?>
-					<p> <input type="text" name='meta_horse_pedigree[<?php echo $x; ?>]' value='<?php if ($meta_horse_pedigree[$x]) {echo $meta_horse_pedigree[$x];} ?>' /> </p>
+        <p> <input type="text" name='meta_horse_pedigree[<?php echo $x; ?>]' value="<?php if ($meta_horse_pedigree[$x]) {echo htmlentities($meta_horse_pedigree[$x], ENT_QUOTES);} ?>" /> </p>
 			<?php
 				if ($x == 2) {echo '<div class="Clear"></div></div>';}
 				if ($x == 6) {echo '<div class="Clear"></div></div>';}
 				if ($x == 14) {echo '<div class="Clear"></div></div>';}
 				if ($x == 30) {echo '<div class="Clear"></div></div>';}
-			} 
+			}
 			?>
 			<div class="Clear"></div>
 		</div>
 		<div class="Clear"></div>
 	</div>
-	
+
 <?php
 	// create a custom nonce for submit verification later
 	echo '<input type="hidden" name="meta_horse_pedigree_noncename" value="' . wp_create_nonce(__FILE__) . '" />';
@@ -70,9 +70,9 @@ function meta_save_horse_pedigree($post_id) {
 
 	/* authentication passed, save data */
 	// meta data
-	$current_data14 = get_post_meta($post_id, 'meta_horse_pedigree', TRUE); 
+	$current_data14 = get_post_meta($post_id, 'meta_horse_pedigree', TRUE);
 	$new_data14 = $_POST['meta_horse_pedigree'];
-	
+
 	if ($new_data14) {
 		my_meta_clean($new_data14);
 		if ($current_data14) { update_post_meta($post_id,'meta_horse_pedigree',$new_data14); }
